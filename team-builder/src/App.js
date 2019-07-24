6c9fdb7731a7ee2cfa1fd4d787d93f7a63b01e1b
Arvin Agas <arvagas@gmail.com>
2019-07-24 16:44:38 -0400

    split off TeamList data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,52 +1,18 @@
 import React, { useState } from 'react'
 
 import Form from './Form'
+import { TeamList } from './TeamList'
+
 import './App.css'
 
 function App() {
-  const [team, setTeam] = useState([
-  {
-    name: 'Adnan',
-    email: 'adnan@lambda.com',
-    role: 'Front-End',
-  },
-  {
-    name: 'Darien',
-    email: 'darien@lambda.com',
-    role: 'UI',
-  },
-  {
-    name: 'Jade',
-    email: 'jade@lambda.com',
-    role: 'Back-End',
-  },
-  {
-    name: 'Joshua',
-    email: 'joshua@lambda.com',
-    role: 'Front-End',
-  },
-  {
-    name: 'Landry',
-    email: 'landry@lambda.com',
-    role: 'Front-End',
-  },
-  {
-    name: 'Luis',
-    email: 'luis@lambda.com',
-    role: 'Back-End',
-  },
-  {
-    name: 'Zachary',
-    email: 'zachary@lambda.com',
-    role: 'Back-End',
-  },
-])
+  const [team, setTeam] = useState(TeamList)
 
   return (
     <div className="App">
       <Form team={team} setTeam={setTeam} />
       {console.log('Team: ', team)}
-      {team.map((person, index) => (
+      {team.map(person => (
         <div key={person.name}>
           <h1>{person.name}</h1>
           <h3>{person.email}</h3>
---
+++ team-builder/src/TeamList.js
@@ -0,0 +1,37 @@
+export const TeamList = [
+    {
+      name: 'Adnan',
+      email: 'adnan@lambda.com',
+      role: 'Front-End',
+    },
+    {
+      name: 'Darien',
+      email: 'darien@lambda.com',
+      role: 'UI',
+    },
+    {
+      name: 'Jade',
+      email: 'jade@lambda.com',
+      role: 'Back-End',
+    },
+    {
+      name: 'Joshua',
+      email: 'joshua@lambda.com',
+      role: 'Front-End',
+    },
+    {
+      name: 'Landry',
+      email: 'landry@lambda.com',
+      role: 'Front-End',
+    },
+    {
+      name: 'Luis',
+      email: 'luis@lambda.com',
+      role: 'Back-End',
+    },
+    {
+      name: 'Zachary',
+      email: 'zachary@lambda.com',
+      role: 'Back-End',
+    },
+]
