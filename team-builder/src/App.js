b2031073a4cf16d5b1876114a56af2618e745f23
Arvin Agas <arvagas@gmail.com>
2019-07-24 16:55:47 -0400

    passed person's properties when edit button is clicked

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -7,6 +7,7 @@ import './App.css'
 
 function App() {
   const [team, setTeam] = useState(TeamList)
+  const [memberToEdit, setMemberToEdit] = useState()
 
   return (
     <div className="App">
@@ -17,6 +18,7 @@ function App() {
           <h1>{person.name}</h1>
           <h3>{person.email}</h3>
           <p>{person.role}</p>
+          <button onClick={() => setMemberToEdit(person)}>Edit</button>
         </div>
       ))}
     </div>
